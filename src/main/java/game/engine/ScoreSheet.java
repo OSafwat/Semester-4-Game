f876d2f897f4fa3811769affa6f42f25e81f9084
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-05-02 22:48:06 +0400

    added scoresheet instance variables

--- src/main/java/game/engine/ScoreSheet.java
+++ src/main/java/game/engine/ScoreSheet.java
@@ -1,5 +1,13 @@
 package game.engine;
 
+import game.collectibles.Lion;
+import game.collectibles.Phoenix;
+
 public class ScoreSheet {
+    Hydra hydra;
+    Phoenix phoenix;
+    Lion lion;
+    Dragon dragon;
+    Gaia gaia;
     
 }
