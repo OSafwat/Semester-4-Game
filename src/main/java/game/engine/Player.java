d783b3cd16fa47d9ef4789342a91320f96953f86
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-05-02 22:45:02 +0400

    added player class instance variables

--- src/main/java/game/engine/Player.java
+++ src/main/java/game/engine/Player.java
@@ -1,8 +1,14 @@
 package game.engine;
+import game.collectibles.ElementalCrest;
+import game.collectibles.TimeWarp;
 import game.engine.enums.*;
 
 public class Player {
     PlayerStatus playerstate;
     GameScore gamescore;
+    ScoreSheet scoresheet;
+    ElementalCrest elementalCrest;
+    TimeWarp timeWarp;
+
 
 }
