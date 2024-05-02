2e838bd4528defb6fc53f9e934552c793d9da72b
A-Kirat <ad.kirat@gmail.com>
2024-05-03 01:02:28 +0200

    created check move in GreenDice class

--- src/main/java/game/dice/GreenDice.java
+++ src/main/java/game/dice/GreenDice.java
@@ -1,5 +1,6 @@
 package game.dice;
 import game.creatures.*;
+import game.creatures.ExtraClasses.Guardians;
 public class GreenDice extends Dice {
     private int score;
     private Gaia gaia;
@@ -11,6 +12,20 @@ public class GreenDice extends Dice {
 
     public int getScore(){
         return score;
+    }
+
+
+
+    public boolean checkMove(Dice dice, Creature creature){
+        GreenDice greendie = (GreenDice) dice;
+        Gaia gaiaCreature =(Gaia) creature;
+        int greenValue = greendie.getValue();
+        Guardians speceficGuardian = gaiaCreature.getGuardians(greenValue);
+        if(speceficGuardian.isDead())
+            return false;
+        else
+        return true;
+
     }
 
 
