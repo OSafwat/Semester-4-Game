b98f0eed3657da45a8c9983dfcdd6b098c81222b
A-Kirat <ad.kirat@gmail.com>
2024-05-03 00:10:35 +0200

    created Gaia constructor

--- src/main/java/game/creatures/Gaia.java
+++ src/main/java/game/creatures/Gaia.java
@@ -5,7 +5,25 @@ import game.creatures.ExtraClasses.Guardians;
 public class Gaia extends Creature{
 
     Guardians [][]  gaiaGuardians;
-    
+
+    public Gaia(){
+        gaiaGuardians = new Guardians[3][4];
+
+        int c =1;
+        for(int i=0;i<gaiaGuardians.length;i++){
+            for(int j=0;j<gaiaGuardians[i].length;j++){
+                gaiaGuardians[i][j]= new Guardians(c);
+                c++;
+
+            }
+
+        }
+        gaiaGuardians[0][0].kill();
+
+
+    }
+
+
 
 
 
