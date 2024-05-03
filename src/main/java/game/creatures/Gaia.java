5f2a1673d8d9eb4b268389fa5b27c649ee95b329
A-Kirat <ad.kirat@gmail.com>
2024-05-03 17:15:57 +0200

    moved all methods in dice to gaia class

--- src/main/java/game/creatures/Gaia.java
+++ src/main/java/game/creatures/Gaia.java
@@ -1,12 +1,18 @@
 package game.creatures;
 
 import game.creatures.ExtraClasses.Guardians;
+import game.dice.Dice;
+import game.dice.GreenDice;
 
 public class Gaia extends Creature{
 
     private Guardians [][]  gaiaGuardians;
     private int alliveGuardians;
     private int deadGuardians;
+    private int score;
+    private int [] scores ={1,2,4,7,11,16,22,29,37,46,56};
+    private boolean [] row={false,false,false};
+    private boolean [] col = {false,false,false,false};
 
     public Gaia(){
         gaiaGuardians = new Guardians[3][4];
@@ -24,10 +30,35 @@ public class Gaia extends Creature{
         }
         gaiaGuardians[0][0].kill();
 
+    }
 
+
+     // EXP mehtod to get the score of the realm
+     public int getScore(){
+        return score;
+    }
+
+    // EXP methos to update the score of the realm
+    private void updateScore(){
+        int dead = this.getDeadGuardians()-1;
+        score= scores[dead];
 
     }
 
+
+
+    // EXP checks if a given move is possible
+    public boolean checkMove(Dice dice, Creature creature){
+        GreenDice greendie = (GreenDice) dice;
+        // ASUM assuming getValue done in the dice class add white
+        int greenValue = greendie.getValue();
+        Guardians speceficGuardian = this.getGuardians(greenValue);
+        if(speceficGuardian.isDead())
+            return false;
+        else
+        return true;
+
+    }
 
 
 
@@ -57,21 +88,6 @@ public class Gaia extends Creature{
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // gets a specific guardian row position in the Gaia
 public int getGuardiansRow(int c){
@@ -149,7 +165,9 @@ public int getDeadGuardians(){
     return deadGuardians;
 }
 
-// checks if all gueadiiand in a given col are dead if yes then true
+
+
+    // checks if all guardians in a given col are dead if yes then true
 public boolean checkCol(int col){
     for(int i=0;i<3;i++){
         if(!gaiaGuardians[i][col].isDead())
@@ -160,7 +178,7 @@ public boolean checkCol(int col){
     return true;
 }
 
-// checks if all gueadiiand in a given row are dead if yes then true
+// checks if all guardians in a given row are dead if yes then true
 public boolean checkRow(int row){
     for(int i=0;i<4;i++){
         if(!gaiaGuardians[row][i].isDead())
@@ -170,6 +188,159 @@ public boolean checkRow(int row){
     }
     return true;
 }
+
+
+// EXP  update the  instance  variable col accordingly
+private  void updateCol(int c){
+         
+    if (this.checkCol(c))
+        col[c]= true;
+}
+
+// EXP check if a row is already killed and update the  instance  variable col accordingly
+private  void updateRow(int r){
+     
+    if (this.checkRow(r)){    
+        row[r]= true;
+     
+    }
+               
+}
+
+
+
+
+ // EXP gives the respective bonus for each col
+    // IMP this will be changed when collectables classes are done
+    // ASUM here I wrote stings but when the leader finish the classes this will be void and replace strings with method.
+    private String whichCollectableCol(int c){
+        switch (c) {
+            case 0: return "Time Warp";
+            case 1: return "Blue_Bonus";
+            case 2 : return "Magenta_Bonus";
+            case 3 : return "Arcane_Power";
+            default:
+                return "Invalid";
+        }
+
+    }
+
+      // EXP gives the respective bonus for each roe
+    // IMP this will be changed when collectables classes are done
+      private String whichCollectableRow(int r){
+        switch (r) {
+            case 0: return "Yellow_Bonus";
+            case 1: return "Red_Bonus";
+            case 2 : return "Elemntal_Crest";
+            default:
+                return "Invalid";
+        }
+
+   
+  
+}
+
+
+
+// EXP executing a given move
+    public boolean makeMove(Dice dice, Creature creature){
+        
+        if(!checkMove(dice, creature))
+            return false;
+        else{
+            GreenDice greendie = (GreenDice) dice;
+            Gaia gaiaCreature = this.gaia;
+            // ASUM assuming getValue done in the dice class
+            int greenValue = greendie.getValue();
+            Guardians speceficGuardian = gaiaCreature.getGuardians(greenValue);
+            gaiaCreature.killGaiaGuardian(speceficGuardian);
+            updateScore();
+            int colToCheck = gaiaCreature.getGuardiansCol(greenValue);
+            int rowToCheck = gaiaCreature.getGuardiansRow(greenValue);
+            updateCol(colToCheck);
+            updateRow(rowToCheck);
+            if(row[rowToCheck]== false && col[colToCheck]==false)
+            return true;
+            else if(row[rowToCheck]== true && col[colToCheck]==false){
+                String act = whichCollectableRow(rowToCheck);
+                // ADD THE CODE OF THE BONUS OR POWER RESPECTIVELY
+                // IMP this will be changed when collectables classes are done
+                // I will need to change in the whichCollectableRow(rowToCheck)
+                return true;
+            }
+            else if(row[rowToCheck]== false && col[colToCheck]==true){
+                String act = whichCollectableCol(colToCheck);
+                // ADD THE CODE OF THE BONUS OR POWER RESPECTIVELY
+                // IMP this will be changed when collectables classes are done
+                // I will only need to change  in the whichCollectableCol(colToCheck);
+                return true;
+            }
+            else{
+                //COMPLETE  create  prioirity method which excutes based on priority 
+                //and modify  whichCollectableCol accordingly
+
+                return true;
+
+
+
+
+            }
+
+
+
+            
+
+        
+        }
+
+
+
+    }
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
