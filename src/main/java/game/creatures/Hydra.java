809b63d36e995d538c308ddc0eadb115441a31b3
AhmedHAEM <ahmedhaem2004@gmail.com>
2024-05-03 21:23:07 +0200

    Defined two private stacks representing the 2 Hydra Serpents.

--- src/main/java/game/creatures/Hydra.java
+++ src/main/java/game/creatures/Hydra.java
@@ -1,5 +1,9 @@
 package game.creatures;
 
 public class Hydra extends Creature{
-    
+    // Create two stacks representing the two serpents.
+    private static Stack<Integer>() FirstSerpent = new Stack<Integer>();
+    FirstSerpent.push(5); FirstSerpent.push(4); FirstSerpent.push(3); FirstSerpent.push(2); FirstSerpent.push(1); 
+    private static Stack<Integer>() SecondSerpent = new Stack<Integer>();
+    SecondSerpent.push(6); SecondSerpent.push(5); SecondSerpent.push(4); SecondSerpent.push(3); SecondSerpent.push(2); SecondSerpent.push(1); 
 }
