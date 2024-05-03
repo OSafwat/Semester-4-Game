89996394681517555e80d8e433c0c95dc18ecba3
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-03 20:17:41 +0200

    created pointMap to assist me in point calculation, and created the initPointMap method

--- src/main/java/game/creatures/Dragon.java
+++ src/main/java/game/creatures/Dragon.java
@@ -1,15 +1,12 @@
 package game.creatures;
 
-import game.creatures.ExtraClasses.DragonData;
-import game.engine.Move;
-
 public class Dragon extends Creature{
-
     public Integer face;
     public Integer wings;
     public Integer tail;
     public Integer heart;
     public Dragon[] Dragon;
+    public Integer[] pointMap;
 
     public Dragon(){
         Dragon = new Dragon[4];
@@ -17,6 +14,7 @@ public class Dragon extends Creature{
         Dragon[1] = new Dragon(6, 1, null, 3);
         Dragon[2] = new Dragon(5, null, 2, 4);
         Dragon[3] = new Dragon(null, 5, 4, 6);
+        initPointMap();
     }
 
     public Dragon (Integer face, Integer wings, Integer tail, Integer heart){
@@ -24,6 +22,14 @@ public class Dragon extends Creature{
         this.wings = wings;
         this.tail = tail;
         this.heart = heart;
+    }
+
+    public void initPointMap(){
+        pointMap = new Integer[4];
+        pointMap[0] = 10;
+        pointMap[1] = 14;
+        pointMap[2] = 16;
+        pointMap[3] = 20;
     }
 
     @Override
