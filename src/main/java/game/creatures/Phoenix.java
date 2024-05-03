dd004091384c9196e0bf681069b07686a4f52904
nesegemaa <Xxnour998xX@gmail.com>
2024-05-04 01:41:00 +0200

    Pressed Ctrl+. to add all the methods that should be overriden from the Creature (super) class

--- src/main/java/game/creatures/Phoenix.java
+++ src/main/java/game/creatures/Phoenix.java
@@ -1,5 +1,28 @@
 package game.creatures;
 
+import game.dice.Dice;
+import game.engine.Move;
+
 public class Phoenix extends Creature{
+
+    @Override
+    public int getElementalCrest() {
+    }
+
+    @Override
+    public String getScoreSheet() {
+    }
+
+    @Override
+    public boolean checkMove(Dice dice) {
+    }
+
+    @Override
+    public boolean makeMove(Dice dice) {
+    }
+
+    @Override
+    public Move[] getAllPossibleMoves() {
+    }
     
 }
