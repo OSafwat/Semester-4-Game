d936da621e7bbb06428e8ece1ac7940582955544
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-10 23:50:49 +0200

    Fixed typos in Player class in the getAllPossibleMoves() method, made it returna Move[] directly Fixed error in initalizing moves in the Hydra class

--- src/main/java/game/engine/CLIGameController.java
+++ src/main/java/game/engine/CLIGameController.java
@@ -414,7 +414,7 @@ public class CLIGameController {
     }
     // move methods
     public Move[] getAllPossibleMoves(Player player) {
-        return player.getAllPossiblMoves();
+        return player.getAllPossibleMoves();
     }
     public Move [] getPossibleMovesForAvailableDice(Player player){
         ArrayList<Move> result = new ArrayList<>();
@@ -425,11 +425,11 @@ public class CLIGameController {
         return (Move [])result.toArray();
     }
     public Move[] getPossibleMovesForADie(Player player, Dice dice){
-        ArrayList<Move> playerAllMoves= player.getAllPossibleMoves();
+        Move[] playerAllMoves= player.getAllPossibleMoves();
         ArrayList<Move> result = new ArrayList<>();
-        for (Move move : playerAllMoves) {
-            if (move.compareTo(dice)==0){
-                result.add(move);
+        for (int i = 0; i < playerAllMoves.length; i++) {
+            if (playerAllMoves[i].compareTo(dice)==0){
+                result.add(playerAllMoves[i]);
             }
         }
         Move [] finalResult = new Move[result.size()];
--- src/main/java/game/engine/Player.java
+++ src/main/java/game/engine/Player.java
@@ -14,7 +14,7 @@ public class Player {
     //private ElementalCrest elementalCrest;
     private ArrayList<ArcaneBoost> arcaneBoosts;
     private ArrayList<TimeWarp> timeWarps;
-    ArrayList <Move> allPossiblMoves ;
+    Move[] allPossibleMoves ;
 
 
     public void setName(String name){
@@ -25,18 +25,23 @@ public class Player {
         this.playerStatus= status;
         this.arcaneBoosts=scoreSheet.getAllArcaneBoosts();
         this.timeWarps=scoreSheet.getAllTimeWarps();
-        allPossiblMoves = getAllPossibleMoves();
+        allPossibleMoves = getAllPossibleMoves();
         gameScore = new GameScore();
     }
 
-    public ArrayList<Move> getAllPossibleMoves(){
+    public Move[] getAllPossibleMoves(){
         ArrayList<Move> allMoves= new ArrayList<>();
         allMoves.addAll(scoreSheet.getCreatureByColor(RealmColor.RED).getAllPossibleMoves());
         allMoves.addAll(scoreSheet.getCreatureByColor(RealmColor.GREEN).getAllPossibleMoves());
         allMoves.addAll(scoreSheet.getCreatureByColor(RealmColor.BLUE).getAllPossibleMoves());
         allMoves.addAll(scoreSheet.getCreatureByColor(RealmColor.MAGENTA).getAllPossibleMoves());
         allMoves.addAll(scoreSheet.getCreatureByColor(RealmColor.YELLOW).getAllPossibleMoves());
-        return allMoves;
+        Move[] res = new Move[allMoves.size()];
+        for (int i = 0; i < allMoves.size(); i++) {
+            res[i] = allMoves.get(i);
+        }
+        allPossibleMoves = res;
+        return res;
     }
 
     public String getName(){
@@ -67,12 +72,8 @@ public class Player {
     public ArrayList<ArcaneBoost> getArcaneBoosts(){
         return this.arcaneBoosts;
     }
-
-    public Move [] getAllPossiblMoves(){
-        return (Move[])this.allPossiblMoves.toArray();
-    }
     public void updateAllPossibleMoves(){
-        this.allPossiblMoves = getAllPossibleMoves();
+        this.allPossibleMoves = getAllPossibleMoves();
     }
     
 
