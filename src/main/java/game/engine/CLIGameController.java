96baa8ba0ca76ec66154b8d02537d309e8b968e9
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-05-10 23:00:08 +0200

    trying to solve the score management

--- src/main/java/game/engine/CLIGameController.java
+++ src/main/java/game/engine/CLIGameController.java
@@ -264,6 +264,7 @@ public class CLIGameController {
                 }
             }
         }   
+        
         scanner.close();
     }
     public Creature getCreatureToAttacByColor(int choice, ScoreSheet scoresheet){
@@ -442,7 +443,7 @@ public class CLIGameController {
 
     // makeMove(new player(), new Move(new RedDice(), new Gaia()))
     public boolean makeMove(Player player, Move move)  {
-        player.updateAllPossibleMoves();
+        player.updateAllPossibleMoves();  //may need to be changed
         try {
             if (move.getCreature() instanceof Gaia) {
                 GreenDice greenDice = (GreenDice) this.gameBoard.getGreen();
@@ -452,6 +453,7 @@ public class CLIGameController {
                 int whiteVal = whiteDice.getValue();
                 greenDice.setRealValue(greenVal + whiteVal);
             }
+            player.updateGameScore();
             return move.getCreature().makeMove(move.getDice());
         } catch (BonusException bException) {
             RealmColor theBonusColor = bException.getRealmColor();
@@ -462,6 +464,7 @@ public class CLIGameController {
                 if (!(numberToAttackWith > 6 || numberToAttackWith < 1)){
                     Creature firstCreature = player.getScoreSheet().getCreatureByColor(theBonusColor);
                     Move firstBonusmove = new Move(new Dice(numberToAttackWith), firstCreature);
+                    player.updateGameScore();
                     return makeMove(player, firstBonusmove);    
                 }else{
                     System.out.println("please enter a valid number");
@@ -478,8 +481,9 @@ public class CLIGameController {
                 if (!(firstNumberToAttackWith > 6 || firstNumberToAttackWith < 1)){
                     Creature firstCreature = player.getScoreSheet().getCreatureByColor(theFirstBonusColor);
                     Move firstBonusmove = new Move(new Dice(firstNumberToAttackWith), firstCreature);
-                    if (makeMove(player, firstBonusmove))
-                        break;
+                    if (makeMove(player, firstBonusmove)){
+
+                        break;}
                     else{
                         System.out.println("please choose a valid move");
                     }
@@ -488,7 +492,7 @@ public class CLIGameController {
                     System.out.println("please enter a valid number");
                 }
             } while (true);
-
+            //player.updateGameScore();
             int secondNumberToAttackWith =0;
             do{
                 System.out.println("please enter the number to attack the " + theSecondBonusColor + " realm with: ");
@@ -506,10 +510,12 @@ public class CLIGameController {
                     System.out.println("please enter a valid number");
                 }
             } while (true);
+            player.updateGameScore();
             return true;
         }
         catch (InvalidMoveException Im){
             System.out.println("i dont get why we would get here");
+            player.updateGameScore();
             return false;
         }
     }
@@ -535,7 +541,12 @@ public class CLIGameController {
     }
 
     public Dice[] getAvailableDice() {
-        return this.gameBoard.getAvailableDice();
+        ArrayList<Dice> temp= this.gameBoard.getAvailableDice();
+        Dice []temp2 = new Dice[temp.size()];
+        for (int index = 0; index < temp.size(); index++) {
+            temp2[index]= temp.get(index);
+        }
+        return temp2;
     }
 
     public Dice[] getForgottenRealmDice() {
