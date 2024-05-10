d7e85234f4cdb07bf1fdabc8b427b31ab5fb9cd2
A-Kirat <ad.kirat@gmail.com>
2024-05-10 23:13:16 +0200

    fixed getscore

--- src/main/java/game/creatures/greenclasses/Gaia.java
+++ src/main/java/game/creatures/greenclasses/Gaia.java
@@ -329,7 +329,7 @@ private  void updateRow(int r){
             int greenValue = greendie.getRealValue();
             Guardians speceficGuardian = this.getGuardians(greenValue);
             this.killGaiaGuardian(speceficGuardian);
-            score=scores[deadGuardians];
+            score=scores[deadGuardians-1];
             int colToCheck = this.getGuardiansCol(greenValue);
             int rowToCheck = this.getGuardiansRow(greenValue);
             updateCol(colToCheck);
