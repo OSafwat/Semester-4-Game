29924e2a9bbecb43b82c255a53bbc11788d4e94f
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-12 05:41:35 +0200

    uncommented the methods in the main class

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Dice Realms: Quest for the Elemental Crests!");
 
-        // CLIGameController cliGameController = new CLIGameController();
-        // cliGameController.startGame();
+        CLIGameController cliGameController = new CLIGameController();
+        cliGameController.startGame();
     }
 }
