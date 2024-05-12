ada1c8387e4717e7699096b8e642ad25b16b1e7b
AhmedHAEM <ahmedhaem2004@gmail.com>
2024-05-12 18:45:57 +0200

    Reverted changes in the pom.xml file.

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,12 +1,16 @@
 package game;
 
 import game.engine.*;
-
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dice Realms: Quest for the Elemental Crests!");
 
-        // CLIGameController cliGameController = new CLIGameController();
-        // cliGameController.startGame();
+        CLIGameController cliGameController = new CLIGameController();
+        try {
+            cliGameController.startGame();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }    
     }
 }
