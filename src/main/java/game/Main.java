695707bf2613d1828e5765843eee4f2a5d0c15cc
AhmedHAEM <ahmedhaem2004@gmail.com>
2024-05-12 18:49:13 +0200

    Removed unnecessary import.

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,7 +1,6 @@
 package game;
 
 import game.engine.*;
-import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dice Realms: Quest for the Elemental Crests!");
