62b1b36b8f2fe446e79b5c57a028d318a6e8229f
AhmedHAEM <ahmedhaem2004@gmail.com>
2024-05-14 21:51:08 +0200

    Defined constructor, global variables, and properties file.

--- src/main/java/game/creatures/BetterLion.java
+++ src/main/java/game/creatures/BetterLion.java
@@ -1,6 +1,10 @@
 package game.creatures;
 
 import java.util.ArrayList;
+import java.util.Properties;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileReader;
 
 import game.collectibles.ArcaneBoost;
 import game.collectibles.TimeWarp;
@@ -10,11 +14,39 @@ import game.exceptions.BonusException;
 import game.exceptions.InvalidMoveException;
 
 public class BetterLion extends Creature{
+    private final Properties properties;
 
+    private int[] scores;
+    private int score;
+    private int lionsKilled;
+
+    public BetterLion() {
+        this.properties = new Properties();
+        try {
+            File config = new File("src/main/resources/config/RadiantSavannaRewards.properties");
+            FileReader configReader = new FileReader(config);
+            this.properties.load(configReader);
+        } catch (IOException e) {
+            System.out.println("Properties file reading failed.");
+            this.properties.setProperty("hit1Reward", null);
+            this.properties.setProperty("hit2Reward", null);
+            this.properties.setProperty("hit3Reward", "TimeWarp");
+            this.properties.setProperty("hit4Reward", null);
+            this.properties.setProperty("hit5Reward", "RedBonus");
+            this.properties.setProperty("hit6Reward", "ArcaneBoost");
+            this.properties.setProperty("hit7Reward", null);
+            this.properties.setProperty("hit8Reward", "ElementalCrest");
+            this.properties.setProperty("hit9Reward", null);
+            this.properties.setProperty("hit10Reward", "MagentaBonus");
+            this.properties.setProperty("hit11Reward", null);
+        }
+
+        scores = new int[11];
+        score = 0;
+    }
     @Override
     public int getElementalCrest() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getElementalCrest'");
+
     }
 
     @Override
