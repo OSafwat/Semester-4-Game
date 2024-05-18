375f5e2120e2bceb0d770b581a28edb2534a687a
A-Kirat <ad.kirat@gmail.com>
2024-05-18 18:40:30 +0200

    added essence bonus

--- src/main/java/game/creatures/greenclasses/Gaia.java
+++ src/main/java/game/creatures/greenclasses/Gaia.java
@@ -38,7 +38,7 @@ public class Gaia extends Creature{
     private String [] colreward;
     private String [] rowreward;
     private String [] defaultcolreward ={"TimeWarp","BlueBonus","MagentaBonus","ArcaneBoost","GreenBonus"};
-    private String [] defaultrowreward={"YellowBonus","RedBonus","ElementalCrest"};
+    private String [] defaultrowreward={"YellowBonus","RedBonus","ElementalCrest","EssenceBonus"};
     //private ArrayList<TimeWarp> timeWarps ;
     //private ArrayList<ArcaneBoost> arcaneBoosts;
     private int elementalCrestCount;
@@ -405,17 +405,17 @@ private  void updateRow(int r){
                 String act2 = whichCollectableRow(rowToCheck);
                 int act1Prtority = this.getPriorityValue(act1);
                 int act2Prtority=this.getPriorityValue(act2);
-                if(act1Prtority==1 && act2Prtority==1){
+                if(act1Prtority==0 && act2Prtority==0){
                     this.applyNotBonusCollectable(act1);
                     this.applyNotBonusCollectable(act2);
                 }
-                else if(act1Prtority>1 && act2Prtority==1){
+                else if(act1Prtority>0 && act2Prtority==0){
                     this.applyNotBonusCollectable(act2);
                     RealmColor  realm= this.getCorrectRealm(act1);
                     throw new BonusException(realm);
 
                 }
-                else if(act1Prtority==1 && act2Prtority>1){
+                else if(act1Prtority==0 && act2Prtority>0){
                     this.applyNotBonusCollectable(act1);
                     RealmColor  realm= this.getCorrectRealm(act2);
                     throw new BonusException(realm);
@@ -605,6 +605,7 @@ private RealmColor getCorrectRealm(String s){
         case "BlueBonus": return RealmColor.BLUE;
         case "MagentaBonus": return RealmColor.MAGENTA;
         case "YellowBonus":return RealmColor.YELLOW;
+        case "EssenceBonus": return RealmColor.WHITE;
         default :return null;
     }
 }
@@ -617,7 +618,8 @@ private int getPriorityValue(String s){
         case "BlueBonus": return 4;
         case "MagentaBonus": return 3;
         case "YellowBonus":return 2;
-        default: return 1;
+        case "EssenceBonus":return 1;
+        default: return 0;
             
     }
 
@@ -635,6 +637,7 @@ private String getCorrectBonusInScore(String s){
         case "TimeWarp" : return"TW";
         case "ArcaneBoost" : return"AB";
         case "ElementalCrest": return"EC";
+        case "EssenceBonus" : return "EB";
         default: return "";
             
     }
