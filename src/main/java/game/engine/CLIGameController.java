efaa74af0ba25f9becf39ca115ac9d69a6da9c0f
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-18 15:24:54 +0200

    added option for the player to skip his turn (should logically be used only if the player hits an infinite loop while playing)

--- src/main/java/game/engine/CLIGameController.java
+++ src/main/java/game/engine/CLIGameController.java
@@ -308,6 +308,16 @@ public class CLIGameController {
         if (moveSet.length == 0)
             throw new NoAvailableMovesException();
         Arrays.sort(diceSet);
+        System.out.println("Would you like to skip your turn?\nIf so, type 'yes', or type 'no' if you do not want to skip your turn.");
+        String skip = "";
+        while (skip.isEmpty()) {
+            skip = scanner.nextLine();
+            if (skip.equals("yes"))
+                return true;
+            if (skip.equals("no"))
+                break;
+            skip = "";
+        }
         boolean valid = false;
         Dice finalDie;
         while (!valid) {
