c2167e63385d843aec4aae10f973f110bb678d68
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-24 22:09:17 +0200

    filled the PlayerActionException with variables and methods

--- src/main/java/game/exceptions/PlayerActionException.java
+++ src/main/java/game/exceptions/PlayerActionException.java
@@ -1,5 +1,17 @@
 package game.exceptions;
 
 public class PlayerActionException extends Exception{
-    
+    private String message;
+
+    public PlayerActionException() {
+        message = "";
+    }
+
+    public PlayerActionException(String message) {
+        this.message = message;
+    }
+
+    public void displayMessage () {
+        System.out.println(message);
+    }
 }
