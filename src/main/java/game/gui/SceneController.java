db106d8f3058de5a6dc1bfc34c44626a19ea8c3e
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-28 22:47:57 +0200

    made a SceneController file that contains an instance of all other scenes, and added getters for some of them

--- src/main/java/game/gui/SceneController.java
+++ src/main/java/game/gui/SceneController.java
@@ -6,10 +6,11 @@ import javafx.scene.image.ImageView;
 public class SceneController {
     public RedScene redScene;
     public GreenScene greenScene;
-    BlueScene blueScene;
-    MagentaScene magentaScene;
-    YellowScene yellowScene;
+    public BlueScene blueScene;
+    public MagentaScene magentaScene;
+    public YellowScene yellowScene;
     public BoardScene boardScene;
+    public MainMenuScene mainMenuScene;
 
     public SceneController () {
         redScene = new RedScene();
@@ -18,9 +19,26 @@ public class SceneController {
         magentaScene = new MagentaScene();
         yellowScene = new YellowScene();
         boardScene = new BoardScene();
+        mainMenuScene = new MainMenuScene();
     }
 
     public ImageView getRedDice () {
         return boardScene.getRedDice();
+    }
+
+    public ImageView getGreenDice () {
+        return boardScene.getGreenDice();
+    }
+
+    public ImageView getBlueDice () {
+        return boardScene.getBlueDie();
+    }
+
+    public ImageView getMagentaDice () {
+        return boardScene.getMagentaDice();
+    }
+
+    public ImageView getYellowDice () {
+        return boardScene.getYellowDice();
     }
 }
