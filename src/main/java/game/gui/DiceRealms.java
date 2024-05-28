51ca562d3e366c6d2d58d97dd087d40b3ec44078
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-28 21:11:59 +0200

    added some imports and removed some unnecessary imports

--- src/main/java/game/gui/DiceRealms.java
+++ src/main/java/game/gui/DiceRealms.java
@@ -1,21 +1,7 @@
 package game.gui;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import game.engine.CLIGameController;
 
 public class DiceRealms extends Application {
     SceneController sceneController;
@@ -41,6 +27,5 @@ public class DiceRealms extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        
     }
 }
