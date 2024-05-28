3767d8645e90d07f1dbf64bd2184e88dd4a90835
Omar Safwat <omarsafsab2004@gmail.com>
2024-05-28 22:52:16 +0200

    also added a setupRealmScene(String realmColor) method that gets the scene from the correct scene (through the scene controller) depending on the input string, as well as a setupGameScene method that sets up the dice scene when the "start game" button is pressed, and added the guiGamecontroller as an instance variable and instantiated it

--- src/main/java/game/gui/DiceRealms.java
+++ src/main/java/game/gui/DiceRealms.java
@@ -1,23 +1,26 @@
 package game.gui;
+import game.dice.Dice;
+import game.engine.GUIGameController;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class DiceRealms extends Application {
+    GUIGameController guiGameController;
     SceneController sceneController;
     Stage primaryStage;
     @Override
     public void start(Stage primaryStage) {
+        guiGameController = new GUIGameController();
         this.primaryStage = primaryStage;
         primaryStage.setY(0);
         primaryStage.setX(0);
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/wizard hat.png")));
-        sceneController = new SceneController();
-        primaryStage.setResizable(false);
 
-        primaryStage.setTitle("Dice Realms Game");
-        primaryStage.setScene(sceneController.boardScene.makeDiceScene());
-        sceneController.getRedDice().setOnMouseClicked(e -> setupRed());
+        sceneController = new SceneController();
+        setupGame();
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
