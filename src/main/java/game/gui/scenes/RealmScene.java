46649217d5bda6c37c57d1841e6d33bc8c04da08
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-05-29 13:33:16 +0200

    added effects to the gobackButton

--- src/main/java/game/gui/scenes/RealmScene.java
+++ src/main/java/game/gui/scenes/RealmScene.java
@@ -2,6 +2,7 @@ package game.gui.scenes;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -11,12 +12,18 @@ public abstract class RealmScene {
     public AnchorPane root;
     public ImageView goBackButton;
     public void createScene() {
+        root = new AnchorPane();
+        Rectangle clip = new Rectangle(300, 200);
+        clip.setArcWidth(30);
+        clip.setArcHeight(30);
+        
         goBackButton = new ImageView(new Image(getClass().getResourceAsStream("/images/BlueGoBackButton.png")));
         goBackButton.setFitWidth(150);
         goBackButton.setFitHeight(150);
         goBackButton.setLayoutX(1730);
         goBackButton.setLayoutY(30);
-
+        goBackButton.setClip(clip);
+        
         root.getChildren().addAll(goBackButton);
         mainScene = new Scene(root,1920,1080);
     };
