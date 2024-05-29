9dabe8201ece6a4831b7aac3f1e6c609fe4aa483
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-05-29 13:03:15 +0200

    added a goback button in the abstract realmScene class

--- src/main/java/game/gui/scenes/RealmScene.java
+++ src/main/java/game/gui/scenes/RealmScene.java
@@ -2,12 +2,25 @@ package game.gui.scenes;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public abstract class RealmScene {
     public Scene mainScene;
     public AnchorPane root;
+    public ImageView goBackButton;
+    public void createScene() {
+        root = new AnchorPane();
+        goBackButton = new ImageView(new Image(getClass().getResourceAsStream("/images/BlueGoBackButton.png")));
+        goBackButton.setFitWidth(150);
+        goBackButton.setFitHeight(150);
+        goBackButton.setLayoutX(1730);
+        goBackButton.setLayoutY(30);
 
-    public void createScene() {};
+        root.getChildren().addAll(goBackButton);
+        mainScene = new Scene(root,1920,1080);
+    };
 
     public Scene getScene() {
         return mainScene;
