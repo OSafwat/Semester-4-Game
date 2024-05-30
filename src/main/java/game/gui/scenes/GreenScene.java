c42d2fd70a0621545eabbaaf195ce0aabc331152
nesegemaa <Xxnour998xX@gmail.com>
2024-05-30 20:20:22 +0200

    Added GreenScene.css and linked it with GreenScene.java. Added also an image for the GreenGaurdian on its own.

--- src/main/java/game/gui/scenes/GreenScene.java
+++ src/main/java/game/gui/scenes/GreenScene.java
@@ -13,14 +13,21 @@ public class GreenScene extends RealmScene {
     public void createScene() {
         root = new AnchorPane();
 
-        backgroundView = new ImageView(new Image(getClass().getResourceAsStream("/images/GreenRealmImages/Terra's_Heartland.png")));
+        backgroundView = new ImageView(new Image(getClass().getResourceAsStream("/images/GreenRealmImages/Terra's Heartland_11.png")));
         backgroundView.setFitWidth(1920);
         backgroundView.setFitHeight(1080);
         backgroundView.setPreserveRatio(false);
         killButton = new Button();
+        killButton.setPrefHeight(700);
+        killButton.setPrefWidth(700);
         root.getChildren().add(backgroundView);
         root.setPadding(javafx.geometry.Insets.EMPTY);
         root.getChildren().add(killButton);
+        // Set the position of the button
+        root.setLeftAnchor(killButton, 764.0);
+        root.setTopAnchor(killButton, 466.0);
+
+        root.getStylesheets().add(getClass().getResource("/GreenScene.css").toExternalForm());
 
         super.createScene();
     }
