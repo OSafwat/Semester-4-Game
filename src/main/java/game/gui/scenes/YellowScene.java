6df9bf2f8b7845643ce6659d11480ef4275a9870
NESEGEMAA <nesegemaa@gmail.com>
2024-06-02 13:27:56 +0200

    Removing the button for attacking in the yellow realm when the realm attacks are depleted.

--- src/main/java/game/gui/scenes/YellowScene.java
+++ src/main/java/game/gui/scenes/YellowScene.java
@@ -47,9 +47,14 @@ public class YellowScene extends RealmScene {
 
     public void changeYellowSceneBackgroundView(String path) {
         try {
-            backgroundView.setImage(new Image(path));
+            lion.setImage(new Image(path));
+            if (!root.getChildren().contains(lion)) root.getChildren().add(lion);
         } catch (NullPointerException e) {
             backgroundView.setImage(new Image("/images/YellowRealmImages/Radiant_Savanna_Destroyed.webp"));
+            if (root.getChildren().contains(lion)) {
+                root.getChildren().clear();
+                root.getChildren().addAll(backgroundView, getGoBackButton());
+            }
         }
     }
 
