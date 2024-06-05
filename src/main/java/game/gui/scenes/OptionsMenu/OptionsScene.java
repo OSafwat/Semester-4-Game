82cd0b5452ce2a7688257a4b7e177678ab8082f2
NESEGEMAA <nesegemaa@gmail.com>
2024-06-05 19:02:47 +0200

    Removed unnecessary class attribute, and added a getter method to get the returnFromOptionsButton for the initialisiation of the return button's event listener in the Main Menu class later.

--- src/main/java/game/gui/scenes/OptionsMenu/OptionsScene.java
+++ src/main/java/game/gui/scenes/OptionsMenu/OptionsScene.java
@@ -5,24 +5,29 @@ import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class OptionsScene {
-    private Scene configMenu;
     private Stage stage;
+    OptionsMenuFXMLController controller;
 
     public Scene createOptionsScene(MediaPlayer mediaPlayer) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("OptionsMenu.fxml"));
         Parent root = loader.load();
         
-        OptionsMenuFXMLController controller = loader.getController();
+        controller = loader.getController();
         controller.setMediaPlayer(mediaPlayer);
         controller.setStage(stage);  // Ensure the controller has access to the stage
         
         return new Scene(root, 1920, 1080);
     }
 
+    public Button getReturnFromOptionsButton() {
+        return controller.getReturnFromOptionsButton();
+    }
+    
     public void setStage(Stage stage) {
         this.stage = stage;
     }
