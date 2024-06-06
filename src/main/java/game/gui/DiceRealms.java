d9b17a0dfc6a2bd2a16f1d0ba12bc5d29ddba5a3
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-06-06 20:08:10 +0200

    changed the handling of opening the grimoire to allow for all realms

--- src/main/java/game/gui/DiceRealms.java
+++ src/main/java/game/gui/DiceRealms.java
@@ -11,7 +11,10 @@ import game.exceptions.ExhaustedResourceException;
 import game.exceptions.NoAvailableMovesException;
 import game.exceptions.PlayerActionException;
 import game.gui.scenes.BoardScene;
+import game.gui.scenes.GreenScene;
 import game.gui.scenes.MagentaScene;
+import game.gui.scenes.RedScene;
+import game.gui.scenes.YellowScene;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -137,7 +140,7 @@ public class DiceRealms extends Application {
         return arr;
     
     }   
-    public void openLeftGrimoire(Object unknownScene) {        
+    public void openLeftGrimoire(AnchorPane root) {        
 
         Player player1 = guiGameController.getPlayer1();
         String arr [] =getInformation(player1); 
@@ -193,21 +196,12 @@ public class DiceRealms extends Application {
         close.setLayoutX(1575);
         close.setLayoutY(50);
         
-        if (unknownScene instanceof MagentaScene){
-            sceneController.magentaScene.addToAnchorPane(bg, textAreaPlayer1,textAreaPlayer2, close);
-
-            close.setOnMouseClicked(e -> {
-                sceneController.magentaScene.removeFromAnchorPane(bg, textAreaPlayer1, textAreaPlayer2, close);
-            });
-        }else if (unknownScene instanceof BoardScene){
-            sceneController.boardScene.addToAnchorPane(bg, textAreaPlayer1,textAreaPlayer2, close);
-
-            close.setOnMouseClicked(e -> {
-                sceneController.boardScene.removeFromAnchorPane(bg, textAreaPlayer1, textAreaPlayer2, close);
-            });
-        }
-
         
+        root.getChildren().addAll(bg, textAreaPlayer1, textAreaPlayer2,close );
+
+        close.setOnMouseClicked(e ->{
+            root.getChildren().removeAll(bg, textAreaPlayer1, textAreaPlayer2, close);
+        });
             
     }      
 
@@ -329,7 +323,7 @@ public class DiceRealms extends Application {
     public void initEventListeners() {
         handleAvailabilityCue(guiGameController.getActivePlayer(), guiGameController.getAvailableDice());
         sceneController.mainMenuScene.getStartGameButton().setOnMouseClicked(e -> startGame());
-        sceneController.boardScene.getLeftGrimoire().setOnMouseClicked(e ->   openLeftGrimoire(sceneController.boardScene));  
+        sceneController.boardScene.getLeftGrimoire().setOnMouseClicked(e ->   openLeftGrimoire(sceneController.boardScene.root));  
         sceneController.mainMenuScene.getExitButton().setOnMouseClicked(e -> primaryStage.close());  //this should close the game when clicked
         sceneController.mainMenuScene.getPvPButton().setOnMouseClicked(e -> startGame());
         sceneController.mainMenuScene.getExitButton().setOnMouseClicked(e -> primaryStage.close());  //this should close the game when clicked
@@ -345,7 +339,14 @@ public class DiceRealms extends Application {
         sceneController.getYellowRealmGoBackButton().setOnMouseClicked(e -> goBackEvent());
         sceneController.getLion().setOnMouseClicked(e -> handleMove(5, 0, 0, null));
         sceneController.getGaiaGuardian().setOnMouseClicked(e -> handleMove(2, 0, 0, null));
-        sceneController.magentaScene.getLeftGrimoire().setOnMousePressed(e -> openLeftGrimoire(sceneController.magentaScene));
+
+        sceneController.magentaScene.getLeftGrimoire().setOnMousePressed(e -> openLeftGrimoire(sceneController.magentaScene.root));
+        sceneController.redScene.getLeftGrimoire().setOnMousePressed(e -> openLeftGrimoire(sceneController.redScene.root));
+        sceneController.blueScene.getLeftGrimoire().setOnMousePressed(e -> openLeftGrimoire(sceneController.blueScene.root));
+        sceneController.greenScene.getLeftGrimoire().setOnMousePressed(e -> openLeftGrimoire(sceneController.greenScene.root));        
+        sceneController.yellowScene.getLeftGrimoire().setOnMousePressed(e -> openLeftGrimoire(sceneController.yellowScene.root));
+
+
 
         /*sceneController.getOptionsButton().setOnMouseClicked((e -> {
             Button button = sceneController.loadOptionsScene();
