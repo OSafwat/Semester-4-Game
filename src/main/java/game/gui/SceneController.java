b809c905e7bd9342121133964760fa42004b4746
AhmedHAEM <ahmedhaem2004@gmail.com>
2024-06-07 03:58:08 +0200

    Suppressed a few unhelpful warnings.

--- src/main/java/game/gui/SceneController.java
+++ src/main/java/game/gui/SceneController.java
@@ -90,26 +90,32 @@ public class SceneController {
         return magentaScene.getPhoenix();
     }
 
+    @SuppressWarnings("exports")
     public Button getOptionsButton() {
         return mainMenuScene.getOptionsButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getStartGameButton() {
         return mainMenuScene.getStartGameButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getPvAIButton() {
         return mainMenuScene.getPvAIButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getPvPButton() {
         return mainMenuScene.getPvPButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getGoBackButton() {
         return mainMenuScene.getGoBackButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getExitButton() {
         return mainMenuScene.getExitButton();
     }
@@ -246,6 +252,7 @@ public class SceneController {
         redScene.changeRedSceneView(paths);
     }
 
+    @SuppressWarnings("exports")
     public Button getRollDiceButton() {
         return boardScene.getRollDiceButton();
     }
@@ -254,38 +261,47 @@ public class SceneController {
         return boardScene.getTimeWarp();
     }
 
+    @SuppressWarnings("exports")
     public Button getReturnFromOptionsButton() {
         return optionsScene.getReturnFromOptionsButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getGameConfigurationButton() {
         return optionsScene.getGameConfigButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getReturnToOptionsButton() {
         return configScene.getReturnToOptionsButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getRedConfigButton() {
         return configScene.getRedConfigButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getGreenConfigButton() {
         return configScene.getGreenConfigButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getBlueConfigButton() {
         return configScene.getBlueConfigButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getMagentaConfigButton() {
         return configScene.getMagentaConfigButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getYellowConfigButton() {
         return configScene.getYellowConfigButton();
     }
 
+    @SuppressWarnings("exports")
     public Button getYellowMultiplierConfigButton() {
         return configScene.getYellowMultiplierConfigButton();
     }
@@ -297,6 +313,7 @@ public class SceneController {
     //     return configButtons;
     // }
 
+    @SuppressWarnings("exports")
     public Button[] getReturnToConfigSceneButtons() {
         Button[] saveButtons = {redConfigScene.getReturnToConfigSceneButton(), greenConfigScene.getReturnToConfigSceneButton(), blueConfigScene.getReturnToConfigSceneButton(), magentaConfigScene.getReturnToConfigSceneButton(),
             yellowConfigScene.getReturnToConfigSceneButton(), yellowMultipliersConfigScene.getReturnToConfigSceneButton()};
