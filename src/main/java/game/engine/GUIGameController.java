6c247bc98b420ff141ac7a97da731d036f8ac057
Omar Safwat <omarsafsab2004@gmail.com>
2024-06-07 04:51:53 +0200

    finalized arcane boost + made the hydra class throw InvalidMoveException

--- src/main/java/game/engine/GUIGameController.java
+++ src/main/java/game/engine/GUIGameController.java
@@ -274,10 +274,8 @@ public class GUIGameController extends CLIGameController {
         return -1;
     }
 
-    public void handleArcaneBoosts(Player player) throws ExhaustedResourceException, PlayerActionException {
+    public void handleArcaneBoosts(Player player) throws ExhaustedResourceException{
         ArrayList<ArcaneBoost> arcaneBoosts = player.getArcaneBoosts();
-        if (currentPlayer.getPlayerStatus().equals(PlayerStatus.PASSIVE))
-            throw new PlayerActionException();
         for (ArcaneBoost arcaneBoost: arcaneBoosts) {
             if (arcaneBoost.getStatus() == RewardStates.ACQUIRED) {
                 arcaneBoost.setStatus(RewardStates.USED);
